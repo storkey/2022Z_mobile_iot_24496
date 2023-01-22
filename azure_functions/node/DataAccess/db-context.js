8c441fca0ce5cfd34517517f565bb79a680a3636
Piotr Bosianek <piotr.bosianek@gmail.com>
2023-01-22 22:28:15 +0100

    prepare functions

--- azure_functions/node/DataAccess/db-context.js
+++ azure_functions/node/DataAccess/db-context.js
@@ -6,17 +6,51 @@ class PeopleDbContext {
         log("PeopleDbContext object has been created.");
         this.log = log;
         this.config = parser(connectionString);
-        this.getPeople = this.getPeople.bind(this);
-    }
+        this.config.options.port = 1433;
+    this.getPeople = this.getPeople.bind(this);
+  }
 
-    async getPeople() {
-        this.log("getPeople function - run")
-        const connection = await new sql.ConnectionPool(this.config).connect();
-        const request = new sql.Request(connection);
-        const result = await request.query('select * from People');
-        this.log("getPeople function - done")
-        return result.recordset;
-    }
+  async getPeople() {
+    this.log("getPeople function - run");
+    const connection = await new sql.ConnectionPool(this.config).connect();
+    const request = new sql.Request(connection);
+    const result = await request.query("select * from People");
+    this.log("getPeople function - done");
+    return result.recordset;
+  }
+
+  async addPerson(name, surname, phoneNumber) {
+    this.log("addPerson function - run");
+    const connection = await new sql.ConnectionPool(this.config).connect();
+    const request = new sql.Request(connection);
+    const result = await request.query(
+      `insert into People (FirstName, LastName, PhoneNumber) values (${name}, ${surname}, ${phoneNumber})`
+    );
+    this.log("addPerson function - done");
+    return result.recordset;
+  }
+
+  async removePerson(personId) {
+    this.log("removePerson function - run");
+    const connection = await new sql.ConnectionPool(this.config).connect();
+    const request = new sql.Request(connection);
+    const result = await request.query(
+      `delete from People where PersonId=${personId}`
+    );
+    this.log("removePerson function - done");
+    return result.recordset;
+  }
+
+  async findSPerson(personId) {
+    this.log("findPerson function - run");
+    const connection = await new sql.ConnectionPool(this.config).connect();
+    const request = new sql.Request(connection);
+    const result = await request.query(
+      `select * from People where PersonId=${personId}`
+    );
+    this.log("findPerson function - done");
+    return result.recordset;
+  }
 }
 
 module.exports = PeopleDbContext;
